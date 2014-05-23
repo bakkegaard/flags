5047a8425822f2ee5769173339c0b9fe02929f9c
Nick Bakkegaard <nickbakkegaard@gmail.com>
2014-05-23 12:21:41 +0200

    added Array.prototype.shuffle

--- flags.js
+++ flags.js
@@ -1,10 +1,28 @@
-Array.prototype.shuffle= function(factor){
-	console.log('todo');
-	//var n= this.length;
-	//for(var i=0;i<n*factor;i++){
-		//Math.random();
-	//}
+Array.prototype.copy= function(){
+	var arr= [];	
+	for(var i=0;i<this.length;i++){
+		arr.push(this[i]);	
+	}
+	return arr;
 }
+Array.prototype.shuffle= function(factor){
+	var arr= this.copy();
+	var length= this.length;
+	getRand= function(){
+		return (Math.floor(Math.random()*Math.pow(2,32))) % length;
+	}
+	for(var i=0;i<this.length*factor;i++){
+		var i= getRand();
+		var j= getRand();
+
+		var temp= arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+	return arr;
+
+}
+
 var flags=[
 	['Afghanistan','kabul','http://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Afghanistan.svg'],
 	['Albanien','Tirana','http://upload.wikimedia.org/wikipedia/commons/3/36/Flag_of_Albania.svg'],
@@ -14,39 +32,46 @@ var flags=[
 	var score=0;
 	var streak=0;
 	var first=true;
+	var random_flags;
 $(function(){
-	flags.shuffle(1);
+	random_flags= flags.shuffle(1);
 	next();
+	$("input").keyup(function(event){
+		if(event.keyCode == 13){
+			guess();
+		}
+	});
 });
+
 
 function reset(){
 	current=0;
 	score=0;
 	streak=0;
 	first=true;
-	flags.shuffle(1);
+	random_flags= flags.shuffle(1);
 }
 
 function next(){
 	if(!first) current++;
 	first=false;
 	
-	if(current==flags.length){
+	if(current==random_flags.length){
 		reset();
 	}
 	$('#land').val('');
 	$('#capital').val('');
 	$('#info').empty();
 	$('#info').append('Score: '+score+', streak: '+streak+'<br>');
-	$('#pic').attr("src",flags[current][2]);
+	$('#pic').attr("src",random_flags[current][2]);
 }
 
 function guess(){
 	var land_guess= $('#land').val().toLowerCase().trim();
 	var capital_guess= $('#capital').val().toLowerCase().trim();
 
-	var land_correct = flags[current][0];
-	var capital_correct= flags[current][1];
+	var land_correct = random_flags[current][0];
+	var capital_correct= random_flags[current][1];
 
 	var land_ok= (land_guess===land_correct.toLowerCase());
 	var capital_ok=(capital_guess===capital_correct.toLowerCase());
